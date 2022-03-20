b53017c21f27b09fa4337343a004f1755edd03a8
Ben <ben@zfben.com>
2022-03-20 15:09:54 +0800

    add nightly

---
+++ build-nightly.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { version } = require('./package.json')
+const { execSync } = require('child_process')
+const { readFileSync, writeFileSync } = require('fs')
+
+const newVersion = version + '-nightly-' + Date.now()
+
+for (const name of [
+  'cli',
+  'server',
+  'web'
+]) {
+  const packagePath = __dirname + '/packages/' + name + '/package.json'
+  const package = JSON.parse(readFileSync(packagePath).toString())
+  package.version = newVersion
+  writeFileSync(packagePath, JSON.stringify(package, null, 2))
+  execSync('npm run build -w packages/' + name, { stdio: 'inherit' })
+  execSync('npm publish -w packages/' + name + ' --access public', { stdio: 'inherit' })
+}
