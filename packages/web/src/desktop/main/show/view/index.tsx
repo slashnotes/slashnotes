5d865382d04189da714b98aaf90113312e8e7124
Ben <ben@zfben.com>
2022-03-20 20:51:43 +0800

    add loading

--- packages/web/src/desktop/main/show/view/index.tsx
+++ packages/web/src/desktop/main/show/view/index.tsx
@@ -9,16 +9,20 @@ import * as runtime from 'react/jsx-runtime.js'
 export function View ({ item }: { item: Item }) {
   const [mdxModule, setMdxModule] = useState<{ default: typeof Fragment }>()
   const Content = mdxModule ? mdxModule.default : Fragment
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     action('view', item).then(data => {
       run(data.body, runtime)
-        .then(mdxModule => setMdxModule(mdxModule))
+        .then(mdxModule => {
+          setMdxModule(mdxModule)
+          setLoading(false)
+        })
         .catch(err => {
           console.error(err)
         })
     })
   }, [item])
 
-  return <div className="view"><MDXProvider><Content /></MDXProvider></div>
+  return <div className="view">{loading ? <div>Loading..</div> : <MDXProvider><Content /></MDXProvider>}</div>
 }
