154bb1ba3233d78c745091d628ca2fb26dda6787
Ben <ben@zfben.com>
2022-03-27 15:44:20 +0800

    update ci

--- packages/md/src/__tests__/build.test.ts
+++ packages/md/src/__tests__/build.test.ts
@@ -1,6 +1,9 @@
 import { build } from '../build'
+import { test } from 'uvu'
+import { is } from 'uvu/assert'
 
-it('should work', () => {
-  expect(build('```ts\nconst a = 1\n```'))
-    .toEqual('<pre data-reactroot=""><code class="hljs language-ts"><span class="hljs-keyword">const</span> a = <span class="hljs-number">1</span>\n</code></pre>')
+test('should work', () => {
+  is(build('```ts\nconst a = 1\n```'), '<pre data-reactroot=""><code class="hljs language-ts"><span class="hljs-keyword">const</span> a = <span class="hljs-number">1</span>\n</code></pre>')
 })
+
+test.run()
