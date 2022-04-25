7ce1d3c8aa2a28cb75e9390e8beaf91cca57f9ea
Ben <ben@zfben.com>
2022-04-25 10:40:47 +0800

    fix cd

--- app/desktop/publish.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { execSync } = require('child_process')
-const { join } = require('path')
-
-execSync(`cp ${join(__dirname, '..', '..', 'packages', 'server')}/dist/index.cjs dist/server.cjs`)
-
-execSync(`cp ${join(__dirname,'..', '..', 'packages', 'md')}/dist/index.cjs dist/md.cjs`)
