09258e821f6937d66e5249d04907357c73406ebe
Ben <ben@zfben.com>
2022-05-05 17:07:05 +0800

    fix style

--- packages/desktop/src/desktop/main/show/editor/index.tsx
+++ packages/desktop/src/desktop/main/show/editor/index.tsx
@@ -1,54 +1,28 @@
 import {
   useContext,
-  useEffect, useRef, useState
+  useEffect,
+  useState
 } from 'react'
-import { monaco } from '@slashnotes/monaco-editor-esm'
 import { action } from 'libs/action'
-import './worker'
 import { DesktopContext, Item } from 'desktop/context'
 
 export function Editor ({ item }: {
   item: Item
 }) {
-  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null)
-  const monacoEl = useRef(null)
   const [value, setValue] = useState<string>()
   const { source } = useContext(DesktopContext)
 
   useEffect(() => {
-    if (monacoEl?.current) {
-      const ed = monaco.editor.create(monacoEl.current, {
-        value: '',
-        language: 'markdown',
-        theme: 'vs-dark',
-        fontSize: 12,
-        tabSize: 2,
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        unicodeHighlight: { ambiguousCharacters: false }
-      })
-
-      setEditor(ed)
-
-      ed.onDidChangeModelContent(() => {
-        setValue(ed.getValue())
-      })
-
-      return () => ed.dispose()
-    }
-  }, [])
-
-  useEffect(() => {
-    if (!item || !editor) return
+    if (!item) return
 
     action<{ body: string }>('file/read', {
       ...item,
       folder: source.path,
     })
       .then(data => {
-        editor.setValue(data.body)
+        setValue(data.body)
       })
-  }, [item, editor])
+  }, [item])
 
   useEffect(() => {
     if (!item || !value) return
@@ -60,8 +34,9 @@ export function Editor ({ item }: {
     })
   }, [value])
 
-  return <div
-    ref={ monacoEl }
+  return <textarea
     className='editor'
-  ></div>
+    value={ value }
+    onChange={ e => setValue(e.target.value) }
+  ></textarea>
 }
